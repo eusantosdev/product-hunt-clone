63dae2714d4d55651051892636aaf6606d03e89e
eusantosdev <eusantosdev@gmail.com>
2026-06-23 19:38:37 -0300

    feat: add html tags in the footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+    return(
+        <footer className="p-26 bg-[#534540] text-sm">
+            <h2>Principais categorias</h2>
+            <div className="grid grid-cols-4 gap-4">
+                <div className="">Card 1</div>
+                <div className="">Card 2</div>
+                <div className="">Card 3</div>
+                <div className="">Card 4</div>
+            </div>
+        </footer>
+    )
+}
