961bc3ed978f0a642a504350ecd652c96281f52b
eusantosdev <eusantosdev@gmail.com>
2026-06-25 16:42:46 -0300

    refactor: refactor header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,32 @@
 export default function Header() {
     return(
-        <header className="container mx-auto p-4 rounded-md text-base flex justify-between">
-            <span>Logo</span>
-            <div className="flex gap-4">
-                <a className="hover:text-[#DA552F]" href="#">Produtos</a>
-                <a className="hover:text-[#DA552F]"href="#">Categorias</a>
-                <a className="hover:text-[#DA552F]"href="#">Sobre</a>
-            </div>
-            <div className="flex gap-4">
-                <a href="#">Login</a>
-                <a href="#">Sing-Up</a>
+        <header className="fixed w-full z-20 top-0 start-0 border-b border-default">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <a className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <span className="self-center text-xl text-heading font-semibold whitespace-nowrap">Product Hunt</span>
+                </a>
+                <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse gap-4">
+                <button type="button" className="text-white bg-[#F4320B] hover:bg-brand-strong box-border border border-transparent focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-3 py-2 focus:outline-none">Sing In</button>
+                <button type="button" className="text-white bg-[#F4320B] hover:bg-brand-strong box-border border border-transparent focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-3 py-2 focus:outline-none">Sing Up</button>
+                    <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-body rounded-base md:hidden hover:bg-neutral-secondary-soft hover:text-heading focus:outline-none focus:ring-2 focus:ring-neutral-tertiary" aria-controls="navbar-sticky" aria-expanded="false">
+                        <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" d="M5 7h14M5 12h14M5 17h14"/></svg>
+                    </button>
+                </div>
+                <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+                    <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-default rounded-base bg-neutral-secondary-soft md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-neutral-primary">
+                        <li>
+                            <a href="#" className="block py-2 px-3 text-heading rounded hover:text-[#F4320B] hover:underline">Produtos</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block py-2 px-3 text-heading rounded hover:text-[#F4320B] hover:underline">Categorias</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block py-2 px-3 text-heading rounded hover:text-[#F4320B] hover:underline">Sobre</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </header>
+
     )
 }
