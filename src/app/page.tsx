cf94d7b77a83b61a906ac948d0f68509b6eb3096
eusantosdev <eusantosdev@gmail.com>
2026-06-26 20:58:59 -0300

    feat: add products list component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import ProductsList from "@/components/ProductsList";
 
 export default function Home() {
   return (
     <>
       <Header/>
+      <ProductsList/>
       <Footer/>
     </>
   );
