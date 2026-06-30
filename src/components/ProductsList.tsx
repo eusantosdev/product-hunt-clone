7036bb5c2fbe0abda9c9058361ed0dd1fdb4053b
eusantosdev <eusantosdev@gmail.com>
2026-06-29 21:27:28 -0300

    feat: add rendering list in categories

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -5,21 +5,11 @@ export default function ProductsList() {
         <section className="container items-center mx-auto p-4 flex justify-between">
             <h3>Tranding Topics</h3>
             <div className="flex gap-4">
-                <button className="p-2 rounded-md bg-[#F87C63] hover:bg-[#F4320B]">
-                    IA
-                </button>
-                <button className="p-2 rounded-md bg-[#F87C63] hover:bg-[#F4320B]">
-                    Marketing
-                </button>
-                <button className="p-2 rounded-md bg-[#F87C63] hover:bg-[#F4320B]">
-                    Produtividade
-                </button>
-                <button className="p-2 rounded-md bg-[#F87C63] hover:bg-[#F4320B]">
-                    SaaS
-                </button>
-                <button className="p-2 rounded-md bg-[#F87C63] hover:bg-[#F4320B]">
-                    Tech
-                </button>
+                {categories.map((categorie) => (
+                    <button key={categorie} className="p-2 rounded-md bg-[#F87C63] hover:bg-[#F4320B]">
+                        {categorie}
+                    </button>
+                ))}
             </div>
         </section>
     )
