75894838d035b17f03692869386b6f3d72c2cf29
eusantosdev <eusantosdev@gmail.com>
2026-07-03 21:07:59 -0300

    refactor: remove header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 import ProductsList from "@/components/ProductsList";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <>
-      <Header/>
       <ProductsList/>
       <Footer/>
     </>
