ffcbf2ff4dce648bce38d399c28671264fde6458
eusantosdev <eusantosdev@gmail.com>
2026-07-04 21:53:09 -0300

    feat: add categories array in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,19 @@
 export default function Footer() {
+    const categories = ["Engenharia e Desenvolvimento", "LLMs", "Produtividade", "Marketing e vendas", "Design e Criatividade", "Social e Comunidade", "Finanças", "Agentes de IA"];
+
     return(
         <footer className="w-full max-w-screen-xl mx-auto p-4">
             <h2 className="font-semibold whitespace-nowrap">
                 Principais categorias
             </h2>
             <div className="grid lg:grid-cols-4 gap-4">
-                <div className="">Card 1</div>
-                <div className="">Card 2</div>
-                <div className="">Card 3</div>
-                <div className="">Card 4</div>
-                <div className="">Card 1</div>
-                <div className="">Card 2</div>
-                <div className="">Card 3</div>
-                <div className="">Card 4</div>
+                {categories.map((category) => (
+                    <div 
+                    key={category}
+                    >
+                    {category}
+                    </div>
+                ))}
             </div>
         </footer>
     )
