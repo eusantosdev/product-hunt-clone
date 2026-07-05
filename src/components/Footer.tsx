6fe0adfa3165eee7894f3c49fdc29ceeb5e850d5
eusantosdev <eusantosdev@gmail.com>
2026-07-05 12:16:22 -0300

    feat: add links in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,12 +7,27 @@ export default function Footer() {
                 <h2 className="font-semibold whitespace-nowrap">
                     Principais categorias
                 </h2>
+
                 <div className="grid lg:grid-cols-4 gap-4 mt-4">
                     {categories.map((category) => (
                         <a href="#" key={category} className="font-semibold whitespace-nowrap hover:text-white">
                             {category}
                         </a>
                     ))}
+                </div>
+
+                <div className="pt-4">
+                    <p>© 2026 Product Hunt Clone</p>
+                    <ul className="flex flex-wrap gap-4">
+                        <li><a href="#">Newsletter</a></li>
+                        <li><a href="#">Newsletter</a></li>
+                        <li><a href="#">Apps</a></li>
+                        <li><a href="#">Sobre</a></li>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">termos</a></li>
+                        <li><a href="#">Privacidade e Cookies</a></li>
+                        <li><a href="#">Anucie</a></li>
+                    </ul>
                 </div>
             </div>
         </footer>
