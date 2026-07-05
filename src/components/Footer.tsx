1604759600d638a6e6215baf4f04f89a2217a606
eusantosdev <eusantosdev@gmail.com>
2026-07-04 21:43:20 -0300

    style: upgrade design in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,14 @@
 export default function Footer() {
     return(
-        <footer className="p-26 bg-[#534540] text-sm">
-            <h2>Principais categorias</h2>
-            <div className="grid grid-cols-4 gap-4">
+        <footer className="w-full max-w-screen-xl mx-auto p-4">
+            <h2 className="font-semibold whitespace-nowrap">
+                Principais categorias
+            </h2>
+            <div className="grid lg:grid-cols-4 gap-4">
+                <div className="">Card 1</div>
+                <div className="">Card 2</div>
+                <div className="">Card 3</div>
+                <div className="">Card 4</div>
                 <div className="">Card 1</div>
                 <div className="">Card 2</div>
                 <div className="">Card 3</div>
