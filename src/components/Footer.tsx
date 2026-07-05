bc9a8e73a55b75d319f19a23bf20f64fae770f8b
eusantosdev <eusantosdev@gmail.com>
2026-07-05 12:03:44 -0300

    refactor: add new div

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,16 +2,18 @@ export default function Footer() {
     const categories = ["Engenharia e Desenvolvimento", "LLMs", "Produtividade", "Marketing e vendas", "Design e Criatividade", "Social e Comunidade", "Finanças", "Agentes de IA"];
 
     return(
-        <footer className="w-full max-w-screen-xl mx-auto p-4 text-[#B4C3D2] bg-[#366899]">
-            <h2 className="font-semibold whitespace-nowrap">
-                Principais categorias
-            </h2>
-            <div className="grid lg:grid-cols-4 gap-4 mt-4">
-                {categories.map((category) => (
-                    <a href="#" key={category} className="font-semibold whitespace-nowrap">
-                        {category}
-                    </a>
-                ))}
+        <footer className="w-full p-4 text-[#B4C3D2] bg-[#366899]">
+            <div className="max-w-screen-xl mx-auto">
+                <h2 className="font-semibold whitespace-nowrap">
+                    Principais categorias
+                </h2>
+                <div className="grid lg:grid-cols-4 gap-4 mt-4">
+                    {categories.map((category) => (
+                        <a href="#" key={category} className="font-semibold whitespace-nowrap hover:text-white">
+                            {category}
+                        </a>
+                    ))}
+                </div>
             </div>
         </footer>
     )
