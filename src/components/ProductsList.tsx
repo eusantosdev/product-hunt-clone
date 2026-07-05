1d795ab27aff7a19122269398b1d610f306ab19c
eusantosdev <eusantosdev@gmail.com>
2026-07-04 22:11:58 -0300

    refactor: refactor style

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,15 +1,16 @@
-"use client";
-
 export default function ProductsList() {
     const categories = ["IA", "Marketing", "Produtividade", "SaaS", "Tech"];
 
     return(
-        <section className="container items-center mx-auto p-4 flex justify-between">
-            <h3>Tranding Topics</h3>
-            <div className="flex gap-4">
-                {categories.map((categorie) => (
-                    <button key={categorie} className="p-2 rounded-md bg-[#F87C63] hover:bg-[#F4320B]">
-                        {categorie}
+        <section className="w-full max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <h3 className="text-heading font-semibold whitespace-nowrap">
+                Trending Topics
+            </h3>
+
+            <div className="flex gap-2 overflow-x-auto scrollber-none">
+                {categories.map((category) => (
+                    <button key={category} className="p-2 rounded-md bg-[#F87C63] hover:bg-[#F4320B] whitespace-nowrap">
+                        {category}
                     </button>
                 ))}
             </div>
