fce9706d93747126d0942726ce76490538d54ec9
eusantosdev <eusantosdev@gmail.com>
2026-07-08 15:18:17 -0300

    refactor: products list component refactor

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,19 +1,11 @@
 export default function ProductsList() {
-    const categories = ["IA", "Marketing", "Produtividade", "SaaS", "Tech"];
-
     return(
-        <section className="w-full max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <h3 className="text-heading font-semibold whitespace-nowrap">
-                Trending Topics
-            </h3>
-
-            <div className="flex gap-2 overflow-x-auto scrollber-none">
-                {categories.map((category) => (
-                    <button key={category} className="p-2 rounded-md bg-[#F87C63] hover:bg-[#F4320B] whitespace-nowrap">
-                        {category}
-                    </button>
-                ))}
+        <main className="w-full">
+            <div className="max-w-screen-xl mx-auto px-4 py-8">
+                <h1 className="text-xl font-bold">
+                    O Próximo Grande App
+                </h1>
             </div>
-        </section>
+        </main>
     )
 }
