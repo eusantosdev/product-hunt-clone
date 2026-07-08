b69f09db2cef6233af69741924c84f3953f0c3e0
eusantosdev <eusantosdev@gmail.com>
2026-07-08 15:04:04 -0300

    style: add styles in component

--- src/components/TrandingTopics.tsx
+++ src/components/TrandingTopics.tsx
@@ -1,9 +1,11 @@
 export default function TrandingTopics() {
     return(
-        <main>
-            <h2>
-                O Próximo Grande App
-            </h2>
+        <main className="w-full">
+            <div className="max-w-screen-xl mx-auto px-4 py-8">
+                <h1 className="text-xl font-bold">
+                    O Próximo Grande App
+                </h1>
+            </div>
         </main>
     )
 }
