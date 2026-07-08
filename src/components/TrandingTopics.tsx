36655275109e612b13938087eb41d14e1e3decf9
eusantosdev <eusantosdev@gmail.com>
2026-07-08 15:45:03 -0300

    style add styles in component

--- src/components/TrandingTopics.tsx
+++ src/components/TrandingTopics.tsx
@@ -2,14 +2,14 @@ export default function TrandingTopics() {
     const categories = ["IA", "Marketing", "Produtividade", "SaaS", "Tech"];
 
     return(
-        <section className="w-full max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <h3 className="text-heading font-semibold whitespace-nowrap">
-                Trending Topics
-            </h3>
+        <section className="w-full max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-4 py-4">
+            <h1 className="text-foreground font-semibold">
+                Principais Tópicos
+            </h1>
 
-            <div className="flex gap-2 overflow-x-auto scrollber-none">
+            <div className="flex gap-2 overflow-x-auto scrollbar-none">
                 {categories.map((category) => (
-                    <button key={category} className="p-2 rounded-md bg-[#F87C63] hover:bg-[#F4320B] whitespace-nowrap">
+                    <button key={category} className="text-white text-sm font-medium p-2 rounded-md bg-[#F87C63] hover:bg-[#F4320B] transition-colors duration-200">
                         {category}
                     </button>
                 ))}
