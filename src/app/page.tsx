d9a886e79a555a2e76747644327816505c90f08a
eusantosdev <eusantosdev@gmail.com>
2026-07-08 15:48:06 -0300

    feat: add tranding topics components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import ProductsList from "@/components/ProductsList";
+import TrandingTopics from "@/components/TrandingTopics";
 import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <>
+      <TrandingTopics/>
       <ProductsList/>
       <Footer/>
     </>
