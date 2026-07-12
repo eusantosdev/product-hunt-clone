1354fe00c980165a560451a205b3e0b74f4afffa
eusantosdev <eusantosdev@gmail.com>
2026-07-12 19:11:48 -0300

    feat: add revieweds products

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,29 @@
+import { revieweds, Reviewed } from "@/data/revieweds"
+
 export default function Sidebar() {
     return(
-        <nav>
-            <h1>Produtos revisados por nós</h1>
+        <nav className="md:border-l-2 border-default border-gray-300 h-full md:px-4">
+            <h1 className="text-xl font-bold text-foreground">
+                Produtos revisados por nós
+            </h1>
+            <ul className="py-4 flex flex-col gap-3">
+                {revieweds.map((reviewed) => (
+                    <li key={reviewed.title} className="flex items-center gap-3 hover:cursor-pointer hover:transition-colors duration-200 hover:text-[#F4320B]">
+                        <img src={reviewed.cover} alt={reviewed.title} className="rounded-xl"/>
+                         <div className="flex-1 py-4">
+                            <h3 className="text-sm font-semibold">
+                                {reviewed.title}
+                            </h3>
+                            <p className="text-xs text-foreground">
+                                {reviewed.description}
+                            </p>
+                        </div>
+                        <span className="text-xs font-medium text-foreground">
+                            ★ {reviewed.rating}
+                        </span>
+                    </li>
+                ))}
+            </ul>
         </nav>
     )
 }
