7ee33cdea6edad08777a40a12efc5f510845944a
eusantosdev <eusantosdev@gmail.com>
2026-07-12 13:38:25 -0300

    feat: add layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
 import ProductsList from "@/components/ProductsList";
 import TrandingTopics from "@/components/TrandingTopics";
+import Sidebar from "@/components/Sidebar";
 import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <>
       <TrandingTopics/>
-      <ProductsList/>
+        <div className="max-w-screen-xl mx-auto px-4 py-8">
+          <div className="flex flex-col md:flex-row gap-8">
+            <div className="w-full md:w-2/3">
+              <ProductsList />
+            </div>
+            <div className="w-full md:w-1/3">
+              <Sidebar />
+            </div>
+          </div>
+        </div>
       <Footer/>
     </>
   );
