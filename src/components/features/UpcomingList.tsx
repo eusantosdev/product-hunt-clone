ec790ca1d009ae77ed2af50fba7e48a633b6da5c
eusantosdev <eusantosdev@gmail.com>
2026-07-16 20:46:04 -0300

    refactor: add UpcomingListProps

--- src/components/features/UpcomingList.tsx
+++ src/components/features/UpcomingList.tsx
@@ -1,24 +1,26 @@
-export default function Upcoming() {
-    (
-        <ul className="py-4 flex flex-col">
-            {upcomings.map((upcoming) => (
-                <li key={upcoming.title} className="flex gap-3 items-center py-3">
-                    <div className="flex-1">
-                        <div className="flex items-center justify-between">
-                            <h3 className="text-sm font-bold">
-                                {upcoming.title}
-                            </h3>
-                            <img src={upcoming.cover} alt={upcoming.title} className="w-12 h-12 rounded-xl object-cover shrink-0"/>
-                        </div>
-                        <p className="text-xs text-foreground-muted mt-1">
-                            {upcoming.description}
-                        </p>
-                        <p className="text-xs text-brand-primary mt-1 py-2">
-                            {upcoming.launchDate} · {upcoming.category}
-                        </p>
-                    </div>
-                </li>
-            ))}
-        </ul>
+import { Upcoming } from "@/types";
+
+interface UpcomingListProps {
+    upcoming: Upcoming;
+}
+
+export default function UpcomingList({ upcoming } : UpcomingListProps) {
+    return(  
+        <div className="flex gap-3 items-center py-3">
+            <div className="flex-1">
+                <div className="flex items-center justify-between">
+                    <h3 className="text-sm font-bold">
+                        {upcoming.title}
+                    </h3>
+                    <img src={upcoming.cover} alt={upcoming.title} className="w-12 h-12 rounded-xl object-cover shrink-0"/>
+                </div>
+                <p className="text-xs text-foreground-muted mt-1">
+                    {upcoming.description}
+                </p>
+                <p className="text-xs text-brand-primary mt-1 py-2">
+                    {upcoming.launchDate} · {upcoming.category}
+                </p>
+            </div>
+        </div>
     )
 }
