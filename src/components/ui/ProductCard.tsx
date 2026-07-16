f01933ce544cb74bc0e21fa3fe1775b724428fb7
eusantosdev <eusantosdev@gmail.com>
2026-07-16 20:11:58 -0300

    refactor: refactor div tag and add votebutton component

--- src/components/ui/ProductCard.tsx
+++ src/components/ui/ProductCard.tsx
@@ -1,29 +1,28 @@
-import { products } from "@/data/products";
+import { Product } from "@/types";
 import VoteButton from "./VoteButton";
 
-export default function ProductCard() {
-    return(
-        <ul className="py-4 flex flex-col gap-4">
-            {products.map((product) => (
-                <li key={product.title} className="flex items-center gap-4 py-4 xl:hover:bg-[#eceff1] rounded-xl transition-all duration-200">
-                    <img src={product.cover} alt={product.title} className="rounded-xl"/>
-                    
-                    <div className="flex-1">
-                        <h3 className="font-semibold">{product.rank}. {product.title}</h3>
-                        <p className="text-sm text-gray-600">{product.description}</p>
-                        <div className="flex gap-2">
-                            {product.category.map((cat) => (
-                                <a key={cat} href="#" className="text-xs py-1 hover:underline">
-                                    {cat}
-                                </a>
-                            ))}
-                        </div>
-                    </div>
-                    <VoteButton 
-                        votes={product.votes} 
-                    />
-                </li>
-            ))}
-        </ul>
+interface ProductCardProps {
+    product: Product;
+}
+
+export default function ProductCard({ product } : ProductCardProps) {
+    return(   
+        <div className="flex items-center gap-4 py-4 xl:hover:bg-[#eceff1] rounded-xl transition-all duration-200">
+            <img src={product.cover} alt={product.title} className="rounded-xl"/>
+            <div className="flex-1">
+                <h3 className="font-semibold">{product.rank}. {product.title}</h3>
+                <p className="text-sm text-gray-600">{product.description}</p>
+                <div className="flex gap-2">
+                    {product.category.map((cat) => (
+                        <a key={cat} href="#" className="text-xs py-1 hover:underline">
+                            {cat}
+                        </a>
+                    ))}
+                </div>
+            </div>
+            <VoteButton 
+                votes={product.votes} 
+            />
+        </div>
     )
 }
