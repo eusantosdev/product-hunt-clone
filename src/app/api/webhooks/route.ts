d4edf6523769b6f07a21dd4f1c9576675d1aa32e
eusantosdev <eusantosdev@gmail.com>
2026-07-29 20:34:40 -0300

    feat: add create user logic

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,17 +1,23 @@
 import { verifyWebhook } from '@clerk/nextjs/webhooks'
 import { NextRequest } from 'next/server'
-import { prisma } from '@/lib/prisma'
+import { prisma } from '@/lib/prisma';
 
 export async function POST(req: NextRequest) {
   try {
     const evt = await verifyWebhook(req)
-    const { id } = evt.data
-    const eventType = evt.type
+    const eventType = evt.type;
 
-    if (eventType === "user.created") {
-        console.log('userId:', id)
+    if(eventType === 'user.created') {
+      const { id, created_at, updated_at } = evt.data;
+      const newUser = await prisma.user.create({
+          data: {
+            externalId: id,
+            createdAt: new Date(created_at),
+            updatedAt: new Date(updated_at),
+          }
+      });
+      console.log('New user created: ', newUser);
     }
-
     return new Response('Webhook received', { status: 200 })
   } catch (err) {
     console.error('Error verifying webhook:', err)
