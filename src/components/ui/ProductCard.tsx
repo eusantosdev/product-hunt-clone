81fa7dfe78a2f7f8e544a4bd75f541dde4c9379e
eusantosdev <eusantosdev@gmail.com>
2026-08-17 21:04:49 -0300

    style: add padding

--- src/components/ui/ProductCard.tsx
+++ src/components/ui/ProductCard.tsx
@@ -7,7 +7,7 @@ interface ProductCardProps {
 
 export default function ProductCard({ product } : ProductCardProps) {
     return(   
-        <div className="flex items-center gap-4 py-4 xl:hover:bg-[#eceff1] rounded-xl transition-all duration-200">
+        <div className="flex items-center gap-4 py-4 px-4 xl:hover:bg-[#eceff1] rounded-xl transition-all duration-200">
             <img src={product.cover} alt={product.title} className="rounded-xl"/>
             <div className="flex-1">
                 <h3 className="font-semibold">{product.rank}. {product.title}</h3>
