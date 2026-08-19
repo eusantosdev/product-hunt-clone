2b00d99bc427ffe8d321bbb1ec037820b4bc9798
eusantosdev <eusantosdev@gmail.com>
2026-08-19 20:59:12 -0300

    style: remove text decoration

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -48,7 +48,7 @@ export default function Header() {
           <ul className="flex flex-col font-medium rounded-base bg-neutral-secondary-soft md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-neutral-primary">
             {links.map((link) => (
               <li key={link.label}>
-                <a href={link.href} className="block py-2 px-3 text-heading rounded hover:text-[#F4320B] hover:underline">
+                <a href={link.href} className="block py-2 px-3 text-heading rounded hover:text-[#F4320B]">
                   {link.label}
                 </a>
               </li>
