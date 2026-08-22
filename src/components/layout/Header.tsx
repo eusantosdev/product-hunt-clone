4c6bf8841ba5df6d22621d3a17eb52ff5f889e86
eusantosdev <eusantosdev@gmail.com>
2026-08-21 21:20:38 -0300

    feat: add logo

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,6 +2,7 @@
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
+import logo from '../../public/logo.svg'
 
 import { useState } from "react";
 
